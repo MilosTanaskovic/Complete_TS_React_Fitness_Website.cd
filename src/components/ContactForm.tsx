37f0e63a9dab8c1b5f838f642db987108471163a
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2023-01-16 09:54:07 +0100

    ContactForm Logic

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
 import { Body } from '.';
 
@@ -6,54 +7,82 @@ type ContactFormProps = {
     
 }
 
-const handleSubmit = (e: any) => {
-    e.preventDefault();
-
-    console.log('click');
-}
-
 const inputStyles = `mb-5 w-full rounded-lg bg-primary-300
 px-5 py-3 placeholder-white`;
 const errorStyles = `mt-1 text-primary-500`;
 
 const ContactForm: React.FC<ContactFormProps> = ({}) => {
+
+    const {
+        register,
+        trigger,
+        formState: {errors},
+    } = useForm();
+
+    const handleSubmit = async (e: any) => {
+        const isValid = await trigger();
+        if(!isValid) {
+            e.preventDefault();
+        }
+    }
+
     return (
         <form
             onSubmit={handleSubmit}
             target="_blank"
+            action="https://formsubmit.co/b76790da4a42a2ce12a7354a761c8006"
+            method="POST"
         >
             <input 
                 type="text"
                 placeholder="NAME"
-
+                {...register("name", {
+                    required: true,
+                    maxLength: 100,
+                })}
                 className={inputStyles}
             />
-            <Body className={errorStyles}>
-                error message
-            </Body>
+             {errors.name && (
+                <Body className={errorStyles}>
+                    {errors.name.type === "required" && "This field is required."}
+                    {errors.name.type === "maxLength" && "Max ength is 100 char."}
+                </Body>
+            )}
 
             <input 
                 type="text"
                 placeholder="EMAIL"
-
+                {...register("email", {
+                    required: true,
+                    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
                 className={inputStyles}
             />
 
-            <Body className={errorStyles}>
-                error message
-            </Body>
+            {errors.email && (
+                <Body className={errorStyles}>
+                    {errors.email.type === "required" && "This field is required."}
+                    {errors.email.type === "pattern" && "Invalid email address."}
+                </Body>
+            )}
 
             <textarea 
                 placeholder="MESSAGE"
                 rows={4}
                 cols={50}
-
+                {...register("message", {
+                    required: true,
+                    maxLength: 2000,
+                })}
                 className={inputStyles}
             />
 
-            <Body className={errorStyles}>
-                error message
-            </Body>
+            {errors.message && (
+                <Body className={errorStyles}>
+                    {errors.message.type === "required" && "This field is required."}
+                    {errors.message.type === "maxLength" && "Max ength is 2000 char."}
+                </Body>
+            )}
 
             <button
                 type="submit"
